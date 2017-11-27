a1f42c62250bdf6d78ecb1f0cbdd800070a46a77
Michael Liao <michael_5608@hotmail.com>
2017-11-27 11:24:30 -0800

    Main Screen changes

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 /* 
-  A00892264
   ACIT 3650
 
   Must install!!!
    npm install
    npm install react-navigation --save
    npm install react-native-vector-icons --save
-   npm install react-native-share --save
-   react-native link
-   
-  Optional Dependencies (Was going to use but didn't)
-   npm install react-native-fetch-blob --save
-   npm install create-react-class --save
-   npm install react-native-social-share --save
-   npm install react-native-sketch --save
    
 */
 
